192f9292f26fdf15d749cdc5b74e704e47463266
tompakarlsson <tompa.karlsson@gmail.com>
2021-09-13 23:30:57 +0200

    Snyggat till koden samt lagt till felhantering

--- tokar.py
+++ tokar.py
@@ -6,6 +6,8 @@ def compound_interest(cash, rate, time):
     return round(ci, 2)
 
 #Definiera en funktion som tar in två argument: user_message samt data_type
+#user_message är den sträng som skrivs ut som en input
+#data_type ska vara str, int eller float beroende på vad som efterfrågas
 #Om datatypen som matas in inte stämmer överrens med argumentet data_type returneras ett felmeddelande.
 def input_type_check(user_message:str, data_type):
     while True:
@@ -19,39 +21,10 @@ def input_type_check(user_message:str, data_type):
             continue
     return input_value
 
-#Begär data från användaren genom att skapa en variabel som hänvisar till funktionen input_type_check.
+#Begär data från användaren genom att skapa en variabel som kallar på funktionen input_type_check.
 amount_cash = input_type_check(user_message='Hur mycket pengar har du idag: ', data_type=float)
 yearly_rate = input_type_check(user_message='Ränta i procent: ', data_type=float)
 periods = input_type_check(user_message='Antal år att räkna på: ', data_type=float)
-
-'''while True:
-    try:
-        amount_cash = float(input('Hur mycket pengar har du idag: '))
-        break
-    except ValueError:
-        print('__________________________________________\n')
-        print(' Felaktig datatyp. Använd enbart siffror. \n')
-        print('‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾\n')
-        continue
-while True:
-    try:
-        yearly_rate = float(input('Ränta: '))
-        break
-    except ValueError:
-        print('__________________________________________\n')
-        print(' Felaktig datatyp. Använd enbart siffror. \n')
-        print('‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾\n')
-        continue
-while True:
-    try:
-       periods = int(input('Antal år att räkna på: '))
-       break
-    except ValueError:
-        print('__________________________________________\n')
-        print(' Felaktig datatyp. Använd enbart siffror. \n')
-        print('‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾‾\n')
-        continue
-'''
 
 #Framställning av resultatet
 print('\n---------------RESULTAT---------------')
