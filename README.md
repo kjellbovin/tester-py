6ade72ff14ed911f61faa01c5f5bd4e107e95e46
tompakarlsson <tompa.karlsson@gmail.com>
2021-09-13 13:41:10 +0200

    files edited during liveshare

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Ett gemensamt repo för grupp 3 på Github
+
+# Vi i grupp 3 har gjort så att vi gjort en gemensam kod som löser problemet i filen compound-interest-calculator.py, men bara relativt grundläggande. Sen har de som vill i gruppen tagit koden och kopierat den in i sin egen fil (handle).py och gjort sina egna ändraingar och tillägg.
--- compound interest calculator.py
+++ compound interest calculator.py
@@ -1,12 +1,15 @@
+#Definiera en funktion som sköter uträkningen av ränta på ränta samt returnerar en variabel - ci - avrundad till två decimaler
 def compound_interest(cash, rate, time):
     a = cash * pow((1 + rate/100), time)
     ci = a - cash
     return round(ci, 2)
 
+#Begär input från användaren
 amount_cash = int(input('Hur mycket pengar har du idag: '))
-yearly_rate = int(input('Ränta: '))
+yearly_rate = float(input('Ränta i procent: '))
 periods = int(input('Antal år att räkna på: '))
 
+#Returnera två resultat - dels den sammansätta räntan men också total summa efter ränta
 print('\n---------------RESULTAT---------------')
 print(f'\nSammansatt ränta efter {periods} år: {compound_interest(amount_cash, yearly_rate, periods)}')
 print(f'\nTotalt efter {periods} år: {amount_cash + compound_interest(amount_cash, yearly_rate, periods)}')
