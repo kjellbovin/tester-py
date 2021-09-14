f4a84150b6664813959e2045f2a6cd027af49b9e
kjellbovin <kjell.bovin@characademy.se>
2021-09-14 09:10:29 +0200

    Ändrat ytterligare en gång i kjbov.py

--- kjbov.py
+++ kjbov.py
@@ -1,16 +1,9 @@
-# Kjell Bovin
-#Definiera en funktion som sköter uträkningen av ränta på ränta samt returnerar en variabel - ci - avrundad till två decimaler
-def compound_interest(cash, rate, time):
-    a = cash * pow((1 + rate/100), time)
-    ci = a - cash
-    return round(ci, 2)
+#Joel
 
-#Begär input från användaren
-amount_cash = int(input('Hur mycket pengar har du idag: '))
-yearly_rate = float(input('Ränta i procent: '))
-periods = int(input('Antal år att räkna på: '))
+interest = 1.1
+savings = int(input("Hur mycket vill du spara?"))
+years = int(input("Hur länge vill du spara?"))
 
-#Returnera två resultat - dels den sammansätta räntan men också total summa efter ränta
-print('\n---------------RESULTAT---------------')
-print(f'\nSammansatt ränta efter {periods} år: {compound_interest(amount_cash, yearly_rate, periods)}')
-print(f'\nTotalt efter {periods} år: {amount_cash + compound_interest(amount_cash, yearly_rate, periods)}')
+result = savings*years**interest
+
+print("Du kommer ha",result,"kr efter",years,"år")
